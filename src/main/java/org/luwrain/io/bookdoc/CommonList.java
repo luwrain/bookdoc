072427a98baf1da0cee4aaa98c61ad5b5a5e8a37
Michael Pozhidaev <msp@luwrain.org>
2022-01-11 10:45:52 +0700

    List classes

---
+++ src/main/java/org/luwrain/io/bookdoc/CommonList.java
@@ -0,0 +1,6 @@
+
+package org.luwrain.io.bookdoc;
+
+abstract class CommonList extends CollectionNode
+{
+}
---
+++ src/main/java/org/luwrain/io/bookdoc/UnList.java
@@ -0,0 +1,6 @@
+
+package org.luwrain.io.bookdoc;
+
+public class UnList extends CommonList
+{
+}
