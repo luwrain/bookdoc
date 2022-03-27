1a5efc940d62f4204d7c6b1760029a1e4333aa9d
Michael Pozhidaev <msp@luwrain.org>
2022-03-27 15:29:32 +0700

    Renaming bookdoc.loader to laders

--- src/main/java/org/luwrain/io/bookdoc/Loader.java
+++ src/main/java/org/luwrain/io/bookdoc/Loader.java
@@ -4,13 +4,15 @@ package org.luwrain.io.bookdoc;
 import java.net.URI;
 import java.io.IOException;
 
+import org.luwrain.io.bookdoc.loaders.*;
+
 public abstract class Loader
 {
     public abstract Result load() throws IOException;
 
     static public Loader newDefaultLoader(URI uri)
     {
-	return null;
+	return new DefaultLoader(uri);
     }
 
         static public final class Result
